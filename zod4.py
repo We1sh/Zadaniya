7c7018cac9f60507f8318f8b6b58258fd3859fe9
We1sh <90512906+We1sh@users.noreply.github.com>
2021-10-09 18:26:19 +0300

    Update zod4.py

--- zod4.py
+++ zod4.py
@@ -10,7 +10,7 @@ class Alphabet:
  
     @property 
     def letters_num(self): 
-        return len(self.letters) 
+        print(len(self.letters)) 
  
 class EngAlphabet(Alphabet): 
     __letters_num = 26 
@@ -19,24 +19,25 @@ class EngAlphabet(Alphabet):
         super().__init__('En', list(string.ascii_uppercase)) 
  
     def is_en_letter(self, let): 
-        return let in self.letters 
+         print(let in self.letters) 
  
     @property 
     def letters_num(self): 
-        return EngAlphabet.__letters_num 
+        print(EngAlphabet.__letters_num)
  
     @staticmethod 
     def example(): 
-        return 'Bobi was a good boy!:)"' 
+         print('Bobi was a good boy!')
  
 def main(): 
     eng = EngAlphabet() 
     eng.print() 
     print(eng.letters_num) 
-    print(eng.is_en_letter("F")) 
-    print(eng.is_en_letter("Щ")) 
+    eng.is_en_letter("F") 
+    eng.is_en_letter("Щ") 
     print("English Example:") 
-    print(EngAlphabet.example()) 
+    EngAlphabet.example()
+
  
 if __name__ == "__main__": 
     main()
