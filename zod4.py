3b54ea11f3a93cfb35b1cb0dc6378d75897dc53b
We1sh <90512906+We1sh@users.noreply.github.com>
2021-10-09 18:47:09 +0300

    Update zod4.py

--- zod4.py
+++ zod4.py
@@ -5,12 +5,12 @@ class Alphabet:
         self.lang = lang 
         self.letters = letters 
  
-    def print(self): 
+    def show(self): 
         print(self.letters) 
  
     @property 
-    def letters_num(self): 
-        print(len(self.letters)) 
+    def words_num(self): 
+        return len(self.letters)
  
 class EngAlphabet(Alphabet): 
     __letters_num = 26 
@@ -18,26 +18,26 @@ class EngAlphabet(Alphabet):
     def __init__(self): 
         super().__init__('En', list(string.ascii_uppercase)) 
  
-    def is_en_letter(self, let): 
-         print(let in self.letters) 
+    def is_en_word(self, let): 
+         return let in self.letters
  
     @property 
     def letters_num(self): 
-        print(EngAlphabet.__letters_num)
+        return EngAlphabet.__letters_num
  
     @staticmethod 
-    def example(): 
-         print('Bobi was a good boy!')
+    def sentence(): 
+         return 'Bobi was a good boy!'
  
-def main(): 
-    eng = EngAlphabet() 
-    eng.print() 
-    print(eng.letters_num) 
-    eng.is_en_letter("F") 
-    eng.is_en_letter("Щ") 
+def main():
+    word = EngAlphabet() 
+    word.show() 
+    word.words_num 
+    word.is_en_word("G") 
+    word.is_en_word("П") 
     print("English Example:") 
-    EngAlphabet.example()
+    EngAlphabet.sentence()
 
  
-if __name__ == "__main__": 
-    main()
+if __name__ == "__main__":
+    main() 
